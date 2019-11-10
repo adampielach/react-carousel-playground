72a3efc9ecd9721775b7e46b6e13539a567fd3a3
adampielach <adampielach@gmail.com>
2019-11-10 16:26:26 +0100

    done basic structuring

---
+++ src/Components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Carousel from './Carousel/Carousel';
+import Team from './Team/Team';
+
+function App() {
+  return (
+    <>
+      <Carousel/>
+      <Team/>
+    </>
+  );
+}
+
+export default App;
